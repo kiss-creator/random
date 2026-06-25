17fc9f1fb25c5d1d6504fd3b3276ed127dc22bb2
zde <tripoldought@gmail.com>
2026-06-25 20:52:46 +0600

    Create mess.c

---
+++ mess.c
@@ -0,0 +1,18 @@
+// Date : Jun 24 ; Time : 3 : 47 P.M    
+
+//What I learn C in this 7 days I know I waste many times and put very sort of time in programming but i am practice now what I learn then I got to the new things to learn in C . So, leat's stat :) 
+
+// Mastering the basic of C programming -> 
+// My main learing resource is K&R C which is so boring , slow process but good for serius learner the problem was not the resrouce or other the only problem was me and me ; 
+
+//already have syntax maturity in c bcz of my previous failiure in C and i quit C for some useless reason but this is not gonna happend now -> Ok;
+//variables and data types ;
+//control flow -> if - else if - else , for , while ;
+//Operation -> addition , subtraction, multiplication, division, == , <= , >= , != ; 
+//File handling -> fopen() , fclose(), fgetc mean File Get Character , fprintf() ; 
+//Learn some very basic error handling in C -> like perror() , etc  ;
+//Yeah I learn symbolic constant for fuck magic value ;
+//learn some very basic format specifier but i need to learn more I know but i am so lazy -> %d , %c,  %f ; 
+
+// I know this is not impresive bcz many of this was my past learning concept and I was not learn new thing but I need to practice it again aganin and again for become more mature with very basic things in C then i will go forward after complete my practice ; 
+

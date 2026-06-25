01232f64ff30cdd5abfc0fbee57fa4187eb1fee8
zde <tripoldought@gmail.com>
2026-06-25 20:50:47 +0600

    Create how.s

---
+++ how.s
@@ -0,0 +1,34 @@
+this is the next target that i will finish 
+1 -> msb , lsb  
+2 -> signed vs unsigned 
+3 -> basic of bit flipping and binary addition 
+4 -> ones compliment and 2s compliment 
+5 -> conversion -> base2 -> base10
+6 -> alu vs cu 
+7 -> how cpu works.....cpu cycle , fetch decode and execute 
+8 -> memory alignment ,  only learn about .align n  .......for  riscv asm 
+
+
+_____________________________________________________________signed vs unsigned_________________________________________________
+
+
+
+target 4 bit : 
+
+unsigned -> 0 to (2 to the pow 4) -1 ; 
+
+signed -> 
+    max neg  value -> 0 to -(2 to the pow 4) /2  ; 
+    max pos value  -> 0 to {(2 to the pow 4) / 2} - 1 ;  
+
+
+
+target 8 bit 
+ 
+
+unsigned -> 0 to (2 to the pow 8) -1 
+
+
+signed -> 
+    max neg value -> 0 to -(2 to the pow 8) /2 ; 
+    max pos value -> 0 to {(2 to the pow 8) / 2} -1 ; 
